34a26ddb169ceaef56f7f1ad80bf617f54648ad1
Agnaldo de Oliveira <agnaldo4j@gmail.com>
2021-11-27 02:35:29 -0300

    Initial specs for OrganizationActor

--- apps/usecase/lib/kanban_vision_api/usecase/organization.ex
+++ apps/usecase/lib/kanban_vision_api/usecase/organization.ex
@@ -10,12 +10,20 @@ defmodule KanbanVisionApi.Usecase.Organization do
     GenServer.start_link(__MODULE__, default)
   end
 
-  def push(pid, element) do
-    GenServer.call(pid, {:push, element})
+  def get_all(pid) do
+    GenServer.call(pid, :get_all)
   end
 
-  def fetch(pid) do
-    GenServer.call(pid, :fetch)
+  def get_by_id(pid, domain_id) do
+    GenServer.call(pid, {:get_by_id, domain_id})
+  end
+
+  def get_by_name(pid, domain_name) do
+    GenServer.call(pid, {:get_by_name, domain_name})
+  end
+
+  def add(pid, new_organization = %KanbanVisionApi.Domain.Organization{}) do
+    GenServer.call(pid, {:add, new_organization})
   end
 
   # Server (callbacks)
@@ -26,12 +34,46 @@ defmodule KanbanVisionApi.Usecase.Organization do
   end
 
   @impl true
-  def handle_call(:fetch, _from, state) do
+  def handle_call(:get_all, _from, state) do
     {:reply, {:ok, state}, state}
   end
 
   @impl true
-  def handle_call({:push, element}, _from, state) do
-    {:reply, {:ok, element}, state}
+  def handle_call({:get_by_id, domain_id}, _from, state) do
+    result = case Map.get(state, domain_id) do
+      nil -> {:error, "Organization with id: #{domain_id} not found"}
+      domain -> {:ok, domain}
+    end
+    {:reply, result, state}
+  end
+
+  @impl true
+  def handle_call({:get_by_name, domain_name}, _from, state) do
+    result = internal_get_by_name(state, domain_name)
+    {:reply, result, state}
+  end
+
+  @impl true
+  def handle_call({:add, new_organization}, _from, state) do
+    case internal_get_by_name(state, new_organization.name) do
+      {:error, _} ->
+        new_state = Map.put(state, new_organization.id, new_organization)
+        {:reply, {:ok, new_organization}, new_state}
+      {:ok, _} ->
+        {:reply, {:error, "Organization with name #{new_organization.name} already exists"}, state}
+    end
+  end
+
+  defp internal_get_by_name(state, domain_name) do
+    Map.values(state)
+    |> Enum.filter(fn domain -> domain.name == domain_name end)
+    |> prepare_by_name_result(domain_name)
+  end
+
+  defp prepare_by_name_result(result_list, domain_name) do
+    case result_list do
+      values when values == [] -> {:error, "Organization with name: #{domain_name} not found"}
+      values -> {:ok, values}
+    end
   end
 end
--- apps/usecase/test/kanban_vision_api/usecase/organization_test.exs
+++ apps/usecase/test/kanban_vision_api/usecase/organization_test.exs
@@ -2,13 +2,52 @@ defmodule KanbanVisionApi.Usecase.OrganizationTest do
   use ExUnit.Case, async: true
   doctest KanbanVisionApi.Usecase.Organization
 
-  describe "When start a new organization with empty state" do
-    test "should be empty" do
-      domain = KanbanVisionApi.Domain.Organization.new("Teste")
-      {:ok, pid} = KanbanVisionApi.Usecase.Organization.start_link()
-      assert KanbanVisionApi.Usecase.Organization.fetch(pid) == {:ok, %{}}
-      assert domain.name == "Teste"
+  @tag :usecase_organization
+  describe "When start the system with empty state" do
+    setup [:prepare_context]
+
+    test "should not have any organization", %{actor_pid: pid} do
+      assert KanbanVisionApi.Usecase.Organization.get_all(pid) == {:ok, %{}}
     end
+
+    test "should be able to add organization with name ExampleOrg", %{actor_pid: pid, domain: domain} do
+      assert KanbanVisionApi.Usecase.Organization.add(pid, domain) == {:ok, domain}
+    end
+
+    test "should have only one organization with name ExampleOrg inside", %{actor_pid: pid, domain: domain} do
+      assert KanbanVisionApi.Usecase.Organization.get_all(pid) == {:ok, %{domain.id => domain}}
+    end
+
+    test "should be able to find an organization by id", %{actor_pid: pid, domain: domain} do
+      assert KanbanVisionApi.Usecase.Organization.get_by_id(pid, domain.id) == {:ok, domain}
+    end
+
+    test "should not be able to find an organization with invalid id", %{actor_pid: pid} do
+      template = {:error, "Organization with id: nada not found"}
+      assert KanbanVisionApi.Usecase.Organization.get_by_id(pid, :nada) == template
+    end
+
+    test "should be able to find an organization by name", %{actor_pid: pid, domain: domain} do
+      assert KanbanVisionApi.Usecase.Organization.get_by_name(pid, domain.name) == {:ok, [domain]}
+    end
+
+    test "should not be able to find an organization with a wrong name", %{actor_pid: pid} do
+      template = {:error, "Organization with name: Invalid Name not found"}
+      assert KanbanVisionApi.Usecase.Organization.get_by_name(pid, "Invalid Name") == template
+    end
+
+    test "should not be able to add another organization with name ExampleOrg", %{actor_pid: pid, domain: domain} do
+      template = {:error, "Organization with name ExampleOrg already exists"}
+      assert KanbanVisionApi.Usecase.Organization.add(pid, domain) == template
+    end
+  end
+
+  defp prepare_context(_context) do
+    {:ok, pid} = KanbanVisionApi.Usecase.Organization.start_link()
+    [
+      actor_pid: pid,
+      domain: KanbanVisionApi.Domain.Organization.new("ExampleOrg")
+    ]
   end
 
 end
